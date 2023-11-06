50f6df91e8d9cebee377f3b428aed8651bae732d
changgyun kim <kiki981011@gmail.com>
2023-11-06 21:49:26 +0900

    office home create

---
+++ src/pages/Office/Home/OfficeHome.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import AdminSetting from "./AdminSetting/AdminSetting";
+
+const OfficeHome = () => {
+
+    return(
+        <AdminSetting></AdminSetting>
+        
+    );
+};
+
+export default OfficeHome;
