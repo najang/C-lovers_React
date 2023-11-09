1e75eb6dd4bb9a70d4ffc9dc53a0e5e84fa1c49b
changgyun kim <kiki981011@gmail.com>
2023-11-09 23:09:01 +0900

    orgmanage commit

--- src/pages/Office/OrgManage/OrgManage.js
+++ src/pages/Office/OrgManage/OrgManage.js
@@ -1,94 +1,133 @@
-// OrgManage.js
-import React, { useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import styles from './OrgManage.module.css';
 import { MenuContext } from "../../Office/Office";
 import OrgNode from './OrgNode/OrgNode';
 import axios from 'axios';
 
-
-const OrgBranch = ({ children }) => {
-    const childrenArray = React.Children.toArray(children);
-    const childrenCount = childrenArray.length;
-
-    return (
-        <div className={styles.tree__branch}>
-            {childrenArray.map((child, index) => {
-                const isFirst = index === 0;
-                const isLast = index === childrenCount - 1;
-                const childClassName = `${styles.tree__entry} ${isFirst ? styles['tree__entry--first'] : ''} ${isLast ? styles['tree__entry--last'] : ''}`;
-
-                // Ensure that we are not overwriting any existing className
-                const newProps = {
-                    ...child.props,
-                    className: child.props.className
-                        ? `${child.props.className} ${childClassName}`
-                        : childClassName,
-                };
-                return React.cloneElement(child, newProps);
-            })}
-        </div>
-    );
-}
-
-
 const OrgManage = () => {
-
     const { setSelectedMenu } = useContext(MenuContext);
-    // const { orgData, setOrgData } = useState({
-    //     office: "",
-    //     people : 0,
-    //     department: {[
+    const [officeData, setOfficeData] = useState(null);
+    // 컨텍스트 메뉴 상태
+    const [contextMenu, setContextMenu] = useState(null);
 
-    //     ]
 
-    //     }
-    // })
-
-    // 네비바가 user에 고정되도록 설정
     useEffect(() => {
-        let office = ""
-        let officer = 0;
-        let department = Array();
-        let departmentInfo = []
-        let deptTaskInfo = Array();
+        axios.get("/org/office").then((resp) => {
+            setOfficeData(resp.data);
+            console.log(resp.data);
+        }).catch(error => {
+            console.error("There was an error!", error);
+        });
 
-        axios.get("/org/office")
-            .then((resp) => {
-                office = resp.data.dept_name; // 서버로부터 받은 데이터를 콘솔에 출력
-            })
-        axios.get("/org/office/empCount")
-            .then((resp) => {
-                officer = resp.data;
-            }
-            )
-        
-        axios.get("/org/getDepartment")
-            .then((resp) => {
-                department = resp.data;
-
-                // 모든 부서에 대한 empCount 요청을 생성합니다.
-                const empCountRequests = department.map((dept) => {
-                    return axios.get(`/org/getDepartment/${dept.id}/empCount`);
-                });
-
-                // 모든 요청이 완료될 때까지 기다립니다.
-                return Promise.all(empCountRequests);
-            })
-            .then((empCountResponses) => {
-                // 모든 empCount 요청의 결과를 처리합니다.
-                departmentInfo =  empCountResponses.map((response, index) => {
-                    return { deptOfficer: response.data, ...department[index] };
-                });
-            })
-            .catch((error) => {
-                // 에러 처리
-                console.error("An error occurred:", error);
-            });
         setSelectedMenu("organization");
 
-    }, []);
+        
+        
+
+    }, [setSelectedMenu,contextMenu]); // 의존성 배열에서 contextMenu 제거
+
+    const onContextMenu = (id, position) => {
+
+        const menuWidth = 150; // 가정한 컨텍스트 메뉴의 너비
+        const menuHeight = 100; // 가정한 컨텍스트 메뉴의 높이
 
 
+        const { innerWidth, innerHeight } = window;
+
+
+        const x = position.x + menuWidth > innerWidth ? position.x - menuWidth : position.x;
+
+        const y = position.y + menuHeight > innerHeight ? position.y - menuHeight : position.y;
+
+        setContextMenu({
+            nodeId: id,
+            xPos: x,
+            yPos: y,
+        });
+    };
+
+    // 컨텍스트 메뉴를 렌더링하는 함수
+    const renderContextMenu = () => {
+        if (!contextMenu) return null;
+
+        // 컨텍스트 메뉴 스타일
+        const menuStyle = {
+            position: 'absolute',
+            top: `${contextMenu.yPos}px`,
+            left: `${contextMenu.xPos}px`,
+            backgroundColor: `white`
+        };
+
+        return (
+            <div style={menuStyle} className={styles.context__menu}>
+                {`aaaaaaa`}
+            </div>
+        );
+    };
+
+
+    const renderOrgNodes = (departments) => {
+        return departments.map((dept, index) => {
+            // 부서 레벨에서의 스타일 결정
+            const deptLength = departments.length;
+            const isSoleDept = deptLength === 1;
+            const isFirstDept = index === 0;
+            const isLastDept = index === deptLength - 1;
+            let deptStyles = styles.tree__entry;
+            if (isSoleDept) {
+                deptStyles += ` ${styles['tree__entry__sole']}`;
+            } else {
+                deptStyles += isFirstDept ? ` ${styles['tree__entry--first']}` : '';
+                deptStyles += isLastDept ? ` ${styles['tree__entry--last']}` : '';
+            }
+
+            // 작업그룹 레벨에서의 스타일 결정
+            const taskLength = dept.deptTask ? dept.deptTask.length : 0;
+
+            return (
+                <div key={dept.id} className={deptStyles}>
+                    <OrgNode id={dept.id} name={dept.dept_name} empCount={dept.dept_officer} onContextMenu={onContextMenu} />
+                    {taskLength > 0 && (
+                        <div className={styles.tree__branch}>
+                            {dept.deptTask.map((task, taskIndex) => {
+                                const isSoleTask = taskLength === 1;
+                                const isFirstTask = taskIndex === 0;
+                                const isLastTask = taskIndex === taskLength - 1;
+                                let taskStyles = styles.tree__entry;
+                                if (isSoleTask) {
+                                    taskStyles += ` ${styles['tree__entry__sole']}`;
+                                } else {
+                                    taskStyles += isFirstTask ? ` ${styles['tree__entry--first']}` : '';
+                                    taskStyles += isLastTask ? ` ${styles['tree__entry--last']}` : '';
+                                }
+
+                                return (
+                                    <div key={task.id} className={taskStyles}>
+                                        <OrgNode id={task.id} name={task.task_name} empCount={task.dept_task_officer} onContextMenu={onContextMenu} />
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    )}
+                </div>
+            );
+        });
+    };
+
+
+    // 조직도 전체를 렌더링하는 함수
+    const renderOrgChart = () => {
+        if (!officeData) return null;
+
+        return (
+            <div className={styles.tree}>
+                <OrgNode id={officeData.id} name={officeData.office_name} empCount={officeData.total_officer} onContextMenu={onContextMenu} />
+                <div className={styles.tree__branch}>
+                    {renderOrgNodes(officeData.department)}
+                </div>
+            </div>
+        );
+    };
 
     return (
         <div className={styles.org__mother__div}>
@@ -96,7 +135,7 @@ const OrgManage = () => {
                 <p className={styles.org__title__text}>조직 관리</p>
             </div>
             <hr />
-            <div className={styles.org_detail}>
+            <div className={styles.org__detail}>
                 <table className={styles.org__table}>
                     <thead>
                         <tr>
@@ -114,64 +153,14 @@ const OrgManage = () => {
                     <tbody>
                         <tr>
                             <td colSpan={3} className={styles.tree__td}>
-                                <div className={styles.tree__div}>
-                                    <div className={styles.tree}>
-                                        <OrgNode name="클로버산업" empCount="23" />
-
-                                        <div className={styles.tree__branch}>
-                                            <div className={`${styles.tree__entry} ${styles['tree__entry--first']}`}>
-                                                <OrgNode name="관리부" empCount="6"></OrgNode>
-                                                <div className={styles.tree__branch}>
-                                                    <div className={`${styles.tree__entry} ${styles['tree__entry--first']}`}>
-                                                        <OrgNode name="구매 총무팀" empCount="3" />
-                                                    </div>
-                                                    <div className={`${styles.tree__entry} ${styles['tree__entry--last']}`}>
-                                                        <OrgNode name="재무 회계팀" empCount="3" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className={styles.tree__entry}>
-                                                <OrgNode name="생산부" empCount="10"></OrgNode>
-                                                <div className={styles.tree__branch}>
-                                                    <div className={`${styles.tree__entry} ${styles['tree__entry--first']}`}>
-                                                        <OrgNode name="생산 1팀" empCount="4"></OrgNode>
-                                                    </div>
-                                                    <div className={styles.tree__entry}>
-                                                        <OrgNode name="생산 2팀" empCount="4"></OrgNode>
-                                                    </div>
-                                                    <div className={`${styles.tree__entry} ${styles['tree__entry--last']}`}>
-                                                        <OrgNode name="품질관리팀" empCount="2"></OrgNode>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className={`${styles.tree__entry} ${styles['tree__entry--last']}`}>
-                                                <OrgNode name="영업부" empCount="8"></OrgNode>
-                                                <div className={styles.tree__branch}>
-                                                    <div className={`${styles.tree__entry} ${styles['tree__entry--first']}`}>
-                                                        <OrgNode name="고객지원팀" empCount="3"></OrgNode>
-                                                    </div>
-                                                    <div className={styles.tree__entry}>
-                                                        <OrgNode name="영업 1팀" empCount="3"></OrgNode>
-                                                    </div>
-                                                    <div className={`${styles.tree__entry} ${styles['tree__entry--last']}`}>
-                                                        <OrgNode name="영업 2팀" empCount="2"></OrgNode>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
+                                {renderOrgChart()}
+                                {renderContextMenu()}
                             </td>
                         </tr>
                     </tbody>
                 </table>
-
-
             </div>
-
         </div>
-
     );
 };
 
