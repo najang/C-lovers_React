b1e9a954ab2d069f198528202ab7390c0267efbc
changgyun kim <kiki981011@gmail.com>
2023-11-23 04:29:40 +0900

    temporary commit

--- src/pages/Office/OrgManage/components/OrgNode/OrgNode.js
+++ src/pages/Office/OrgManage/components/OrgNode/OrgNode.js
@@ -4,13 +4,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import styles from "./OrgNode.module.css";
 
-const OrgNode = ({ name, empCount}) => {
+const OrgNode = ({ name, empCount, type, id, officer, onRadioChange, isSelected }) => {
+
+    const radioButtonName = `radio-${type}`;
     // 컴포넌트 렌더링
     return (
         <div className={styles.tree__label}>
             <table>
                 <tbody>
                     <tr>
+                        <td className={styles.tree__radio}>
+                            {radioButtonName !== 'radio-office' && (
+                                <input
+                                    type="radio"
+                                    name={radioButtonName}
+                                    onChange={() => onRadioChange(type, id, name, officer)}
+                                    checked={isSelected}
+                                />
+                            )}
+
+                        </td>
                         <th className={styles.tree__label__element}>{name}</th>
                         <td className={styles.tree__label__element}>
                             <div className={styles.tree__label__info}>
